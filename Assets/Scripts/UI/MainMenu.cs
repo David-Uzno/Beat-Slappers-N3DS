f40cb05759f39c59300cf4120531248f76082076
David <141585041+David-Uzno@users.noreply.github.com>
2025-09-11 23:50:42 -0300

    feat: menú principal

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.N3DS;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
